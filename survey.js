a096b4643ccc53635045043ce99efa252b17a70a
SirGraybon <graydonritchie@gmail.com>
2023-04-12 10:12:46 -0400

    refactored survey.js to hold the answers more efficiently

--- survey.js
+++ survey.js
@@ -5,33 +5,14 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-let name = "";
-let hobby = "";
-let city = "";
-let meal = "";
-let dish = "";
-let power = "";
-
-rl.question('What is your name? ', (answer) => {
-  name = answer;
-  rl.question("Where are you from ", (answer) => {
-    city = answer;
-    rl.question("what is your favourite hobby ", (answer) => {
-      hobby = answer;
-      rl.question("what is your favorite meal(breakfast, lunch, or dinner) ", (answer) => {
-        meal = answer;
-        rl.question("What is your favorouite thing to eat ", (answer) => {
-          dish = answer;
-          rl.question("wjhats your go to show ", (answer) => {
-            power = answer;
-
-
-
-            console.log(`${name}, is on a life long journey to master ${hobby}, and is often found daydreaming about ${power}, while knocking back ${dish} for ${meal}`);
+rl.question('What is your name? ', (name) => {
+  rl.question("Where are you from ", (city) => {
+    rl.question("what is your favourite hobby ", (hobby) => {
+      rl.question("what is your favorite meal(breakfast, lunch, or dinner) ", (meal) => {
+        rl.question("What is your favorouite thing to eat ", (dish) => {
+          rl.question("whats your go to show ", (power) => {
+            console.log(`${name}, is on a life long journey to master ${hobby}, and is often found daydreaming about ${power}, while knocking back ${dish} for ${meal} in ${city}`);
             rl.close();
-
-
-
           });
         });
       });
